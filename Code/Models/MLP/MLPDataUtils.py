6baf0b1ec1826d9159e3ac9677703268f6c9520d
GabeTsai <gthtsai@stanford.edu>
2024-08-21 23:09:19 -0700

    Better training pipeline, remove LSTM and CNN train code

--- Code/Models/MLP/MLPDataUtils.py
+++ Code/Models/MLP/MLPDataUtils.py
@@ -5,75 +5,86 @@ import numpy as np
 import json
 import sys
 sys.path.append('../../')
-from DataUtils import * # Import functions and variables from DataUtils.py
-from Visualizations import histogram
+from DataUtils import prepare_datasets, standardize
+
+from Visualizations import histogram, histogram_single
 
 from pathlib import Path
 
-def prepare_dataset_MLP(data_map, include_qr_nr = False):
+def prepare_dataset_MLP(data_map):
     '''
     Create the input and target datasets. Return as np arrays.
     '''
     #Thresh all of the 2D arrays into 1D arrays
-    qc_autoconv_cloud = np.array(data_map['qc_autoconv_cloud']).flatten().transpose()
-    nc_autoconv_cloud = np.array(data_map['nc_autoconv_cloud']).flatten().transpose()
+    qc_autoconv_cloud = np.array(data_map['qc_autoconv_cloud']).transpose()
+    nc_autoconv_cloud = np.array(data_map['nc_autoconv_cloud']).transpose()
     tke_sgs = np.array(data_map['tke_sgs']).flatten().transpose()
-    if include_qr_nr:
-        qr_autoconv_cloud = np.array(data_map['qr_autoconv_cloud']).flatten().transpose()
-        nr_autoconv_cloud = np.array(data_map['nr_autoconv_cloud']).flatten().transpose()
-        input_data = np.stack((qc_autoconv_cloud, nc_autoconv_cloud, 
-                                qr_autoconv_cloud, nr_autoconv_cloud), axis = 1)
-    else:
-        input_data = np.stack((qc_autoconv_cloud, nc_autoconv_cloud, tke_sgs), axis = 1)
+    input_data = np.stack((qc_autoconv_cloud, nc_autoconv_cloud, tke_sgs), axis = 1)
     
     auto_cldmsink_b_cloud = np.array(data_map['auto_cldmsink_b_cloud']).flatten().transpose()
 
     target_data = auto_cldmsink_b_cloud # (time * height_channels)
-    print(np.count_nonzero(target_data == 0))
     return input_data, target_data
 
-def concat_data(data_maps, model_name, model_folder_path, data_name = '', include_qr_nr = False):
+def concat_data(data_maps, model_name, model_folder_path, data_name = ''):
     '''
     Concatenate data from data maps into tensors
     :param data_maps: list of data maps
     '''
+    from DataUtils import standardize
     input_list = []
     target_list = []
 
     for data_map in data_maps:
-        inputs, targets = prepare_dataset_MLP(data_map, include_qr_nr) 
+        inputs, targets = prepare_dataset_MLP(data_map) 
         input_list.append(inputs) 
         target_list.append(targets) 
 
-    # Concatenate the data
+    # Concatenate and log the data
     input_data = np.concatenate(input_list, axis=0)
     target_data = np.concatenate(target_list, axis=0)
 
-    filter_mask = remove_outliers(target_data)
-    target_data = target_data[filter_mask]
-    input_data = input_data[filter_mask]
+    #Filter out physically impossible values
+    filter = (target_data > 0) & (input_data[:, 0] > 0) & (input_data[:, 1] > 0) & (input_data[:, 2] > 0)
+    input_data = np.log(input_data[filter])
+    target_data = np.log(target_data[filter])
 
-    save_data_info(input_data, target_data, model_folder_path, model_name, data_name)
+    test_percentage = 0.1 # 10% of data used for testing
+    test_size = int(len(input_data) * test_percentage)
+    train_size = len(input_data) - test_size
 
     dims = (0)
-    input_data = min_max_normalize(input_data, dims)
-    target_data = min_max_normalize(target_data, dims)
+    input_data = standardize(input_data, dims)
+    target_data = standardize(target_data)
 
-    print(np.count_nonzero(target_data == 0))
+    indices = np.random.permutation(len(input_data))
+    input_data = input_data[indices]
+    target_data = target_data[indices]
+
+    train_input_data = input_data[:train_size]
+    train_target_data = target_data[:train_size]
+    test_input_data = input_data[train_size:]
+    test_target_data = target_data[train_size:]
+    
+    test_dataset = torch.utils.data.TensorDataset()
+    torch.save(test_dataset, Path(model_folder_path)/ f'{model_name}_test_dataset.pth')
+
     return torch.FloatTensor(input_data), torch.FloatTensor(target_data).unsqueeze(1)
 
-def create_MLP_dataset(data_folder_path, model_name, model_folder_path, include_qr_nr):
+def create_MLP_dataset(data_folder_path, model_name, model_folder_path):
+
     data_maps = prepare_datasets(data_folder_path)
-    inputs, targets = concat_data(data_maps, model_name, model_folder_path, include_qr_nr)
+    inputs, targets = concat_data(data_maps, model_name, model_folder_path)
     return inputs, targets
 
 def main():
     model_name = 'MLP3'
-    inputs, targets = create_MLP_dataset('../../../Data/NetCDFFiles', model_name, f'../../../SavedModels/{model_name}', model_name)
+    inputs, targets = create_MLP_dataset('../../../Data/NetCDFFiles', model_name, f'../../../SavedModels/{model_name}')
     print(inputs.shape, targets.shape)
-    histogram(inputs[:, 0], inputs[:, 0], model_name, 'qc', '../../../Visualizations')
-    histogram(inputs[:, 1], inputs[:, 1], model_name, 'nc', '../../../Visualizations')
-    histogram(inputs[:, 2], inputs[:, 2], model_name, 'tke', '../../../Visualizations')
+    histogram_single(inputs[:, 0], '', model_name, 'qc', '../../../Visualizations')
+    histogram_single(inputs[:, 1], '', model_name, 'nc', '../../../Visualizations')
+    histogram_single(inputs[:, 2], '', model_name, 'tke', '../../../Visualizations')
+    histogram_single(targets, '', model_name, 'target', '../../../Visualizations')
 
 if __name__ == "__main__":
     main()
