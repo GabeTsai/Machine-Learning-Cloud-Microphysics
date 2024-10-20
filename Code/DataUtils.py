fix ensemble data leakage

Code/DataUtils.py
@@ -91,25 +91,35 @@ def min_max_denormalize(data, min_val, max_val):
     """
     return data * (max_val - min_val) + min_val
 
-def standardize(data, mean = None, std = None, dims = None):
+def standardize(data, mean=None, std=None, dims=None):
     """
     Standardize data to have mean 0 and standard deviation 1.
 
     Args:
         data (np.array): Data to standardize.
+        mean (np.array, optional): Array of means to use for standardization. Should match the shape of the columns if provided.
+        std (np.array, optional): Array of standard deviations to use for standardization. Should match the shape of the columns if provided.
         dims (tuple, optional): Dimensions to standardize over. If None, global mean and std used to standardize.
 
     Returns:
         np.array: Standardized data with mean 0 and standard deviation 1.
     """
 
-    if dims is not None:
-        axes = dims
-        mean = np.mean(data, axis=axes, keepdims=True)
-        std = np.std(data, axis=axes, keepdims=True)
-    else:
-        mean = np.mean(data)
-        std = np.std(data)
+    # If mean and std are not provided, calculate them based on dims
+    if mean is None and std is None:
+        if dims is not None:
+            axes = dims
+            mean = np.mean(data, axis=axes, keepdims=True)
+            std = np.std(data, axis=axes, keepdims=True)
+        else:
+            mean = np.mean(data)
+            std = np.std(data)
+    # Ensure mean and std are 1D arrays (for columns)
+    if mean.ndim > 1:
+        mean = np.squeeze(mean)
+    if std.ndim > 1:
+        std = np.squeeze(std)
+
     return (data - mean) / std
 
 def destandardize_single(data, mean, std):
@@ -192,7 +202,6 @@ def create_data_map(data_file_path, hdf=False):
     Returns:
         dict: Dictionary containing prepared data arrays.
     """
-
     cloud_ds = xr.open_dataset(data_file_path, group='DiagnosticsClouds/profiles')
     turb_ds = xr.open_dataset(data_file_path, group='DiagnosticState/profiles')
 

Code/MLPDataUtils.py
@@ -12,6 +12,7 @@ import config
 from pathlib import Path
 
 torch.serialization.add_safe_globals([TensorDataset])
+torch.manual_seed(config.SEED)
 
 def prepare_dataset_MLP(data_map):
     '''
@@ -28,7 +29,7 @@ def prepare_dataset_MLP(data_map):
     target_data = auto_cldmsink_b_cloud # (time * height_channels)
     return input_data, target_data
 
-def concat_data(data_maps, model_name, model_folder_path, data_name = ''):
+def concat_data(data_maps, model_name, model_folder_path, data_name = '', standardize = True):
     '''
     Concatenate data from data maps into tensors
     :param data_maps: list of data maps
@@ -51,71 +52,131 @@ def concat_data(data_maps, model_name, model_folder_path, data_name = ''):
     input_data = np.log(input_data[filter])
     target_data = np.log(target_data[filter])
 
-    rng = np.random.default_rng(42069)
+    rng = np.random.default_rng(config.SEED)
     indices = rng.permutation(len(input_data))
     
     input_data = input_data[indices]
     target_data = target_data[indices]
 
     test_percentage = 0.1 # 10% of data used for testing
+    val_percentage = 0.2
     test_size = int(len(input_data) * test_percentage)
-    train_size = len(input_data) - test_size
+    val_size = int(len(input_data) * val_percentage)
+    train_size = len(input_data) - test_size - val_size
 
     train_input_data = input_data[:train_size]
     train_target_data = target_data[:train_size]
-    print(train_input_data.shape)
+
+    val_input_data = input_data[train_size:train_size + val_size]
+    val_target_data = target_data[train_size:train_size + val_size]
+
+    test_input_data = input_data[train_size + val_size:]
+    test_target_data = target_data[train_size + val_size:]
 
     #Save data for undoing transforms (use train data statistics to prevent leakage)
     save_data_info(train_input_data, train_target_data, model_folder_path, model_name, data_name)
 
-    train_input_data = torch.FloatTensor(standardize(train_input_data))
-    train_target_data = torch.FloatTensor(standardize(train_target_data)).unsqueeze(1)
+    if standardize:
+        train_input_mean = np.mean(train_input_data, axis = 0)
+        train_target_mean = np.mean(train_target_data)
 
-    dims = (0)
-    test_input_data = standardize(input_data[train_size:])
-    test_target_data = standardize(target_data[train_size:])
+        train_input_std = np.std(train_input_data, axis = 0)
+        train_target_std = np.std(train_target_data)
 
-    train_dataset = TensorDataset(torch.FloatTensor(train_input_data), torch.FloatTensor(train_target_data))
-    test_dataset = TensorDataset(torch.FloatTensor(test_input_data), torch.FloatTensor(test_target_data))
-    
-    torch.save(test_dataset, Path(model_folder_path) / f'{model_name}_{data_name}_test_dataset.pth')
+        train_input_data = standardize(train_input_data, dims = (0))
+        train_target_data = standardize(train_target_data)
 
-    return train_dataset
+        val_input_data = standardize(val_input_data, train_input_mean, train_input_std, dims = (0))
+        val_target_data = standardize(val_target_data, train_target_mean, train_target_std)
 
-def create_MLP_dataset(data_folder_path, model_name, model_folder_path, subset):
+        test_input_data = standardize(test_input_data, train_input_mean, train_input_std, dims = (0))
+        test_target_data = standardize(test_target_data, train_target_mean, train_target_std)
+
+    print(train_input_data.shape, train_target_data.shape, val_input_data.shape, val_target_data.shape, test_input_data.shape, test_target_data.shape)
+
+    return train_input_data, train_target_data, val_input_data, val_target_data, test_input_data, test_target_data
+
+def create_MLP_dataset(data_folder_path, model_name, model_folder_path, subset, standardize = True):
     data_maps = prepare_datasets(data_folder_path)
-    train_dataset = concat_data(data_maps, model_name, model_folder_path, subset)
-    return train_dataset
+    train_input_data, train_target_data, val_input_data, val_target_data, test_input_data, test_target_data = concat_data(
+        data_maps, model_name, model_folder_path, data_name = subset, standardize = standardize)
+
+    train_dataset = TensorDataset(torch.FloatTensor(train_input_data), torch.FloatTensor(train_target_data).unsqueeze(1))
+    val_dataset = TensorDataset(torch.FloatTensor(val_input_data), torch.FloatTensor(val_target_data).unsqueeze(1))
+    test_dataset = TensorDataset(torch.FloatTensor(test_input_data), torch.FloatTensor(test_target_data).unsqueeze(1))
+
+    torch.save(test_dataset, Path(model_folder_path) / f'{model_name}_{subset}_test_dataset.pth')
+
+    return train_dataset, val_dataset
 
 def create_ensemble_dataset(data_folder_path, model_name, ensemble_model_name, model_folder_path, regions):
     if model_name != "Ensemble":
         raise ValueError("model_name not Ensemble.")
     train_datasets = []
+    val_datasets = []
     test_datasets = []
     for region in regions:
-        train_dataset = create_MLP_dataset(f'{data_folder_path}/{region}', ensemble_model_name, model_folder_path, region)
+        train_dataset, val_dataset = create_MLP_dataset(f'{data_folder_path}/{region}', ensemble_model_name, model_folder_path, region, standardize = False)
         train_datasets.append(train_dataset)
+        val_datasets.append(val_dataset)
         print(data_folder_path, region)
         test_datasets.append(torch.load(Path(model_folder_path) \
                             / f'{ensemble_model_name}_{region}_test_dataset.pth', weights_only = True))
-
+        
+   # Concatenate datasets
     ensemble_train_dataset = ConcatDataset(train_datasets)
+    ensemble_val_dataset = ConcatDataset(val_datasets)
     ensemble_test_dataset = ConcatDataset(test_datasets)
 
+    # Access inputs and targets from each dataset
+    train_inputs = torch.cat([d.tensors[0] for d in train_datasets], dim=0)
+    train_targets = torch.cat([d.tensors[1] for d in train_datasets], dim=0)
+
+    save_data_info(np.array(train_inputs), np.array(train_targets), model_folder_path, model_name)
+
+    val_inputs = torch.cat([d.tensors[0] for d in val_datasets], dim=0)
+    val_targets = torch.cat([d.tensors[1] for d in val_datasets], dim=0)
+
+    test_inputs = torch.cat([d.tensors[0] for d in test_datasets], dim=0)
+    test_targets = torch.cat([d.tensors[1] for d in test_datasets], dim=0)
+
+    # Calculate mean and std from the training data
+    train_input_mean = torch.mean(train_inputs, dim=0)
+    train_input_std = torch.std(train_inputs, dim=0)
+
+    train_target_mean = torch.mean(train_targets, dim=0)
+    train_target_std = torch.std(train_targets, dim=0)
+
+    train_inputs = standardize(train_inputs, train_input_mean, train_input_std)
+    train_targets = standardize(train_targets, train_target_mean, train_target_std)
+
+    val_inputs = standardize(val_inputs, train_input_mean, train_input_std)
+    val_targets = standardize(val_targets, train_target_mean, train_target_std)
+
+    test_inputs = standardize(test_inputs, train_input_mean, train_input_std)
+    test_targets = standardize(test_targets, train_target_mean, train_target_std)
+
+    ensemble_train_dataset = TensorDataset(train_inputs, train_targets)
+    ensemble_val_dataset = TensorDataset(val_inputs, val_targets)
+    ensemble_test_dataset = TensorDataset(test_inputs, test_targets)
+
+    # Save the test dataset
     torch.save(ensemble_test_dataset, Path(model_folder_path) / f'{model_name}_test_dataset.pth')
 
-    return ensemble_train_dataset
+    return ensemble_train_dataset, ensemble_val_dataset
 
 def main():
     ensemble_model_name = "DeepMLP"
     model_name = "Ensemble" 
-    data_folder_path = config.DATA_FOLDER_PATH
-    model_folder_path = config.MODEL_FOLDER_PATH
+
     regions = config.REGIONS
     subset = regions[0]
-    create_MLP_dataset(data_folder_path, ensemble_model_name, model_folder_path, subset)
+
+    model_folder_path = config.MODEL_FOLDER_PATH
+    
+    # create_MLP_dataset(f"{config.DATA_FOLDER_PATH}/{subset}", ensemble_model_name, model_folder_path, subset)
     model_name = "Ensemble"
-    create_ensemble_dataset(data_folder_path, model_name, ensemble_model_name, model_folder_path, regions)
+    create_ensemble_dataset(config.DATA_FOLDER_PATH, model_name, ensemble_model_name, model_folder_path, regions)
     
 if __name__ == "__main__":
     main()

Code/Train.py
@@ -79,7 +79,7 @@ def choose_model(model_name, input_dim, output_bias, config, **kwargs):
             
             checkpoint = torch.load(full_model_path, map_location=device)
             
-            state_dict = configure_bias(checkpoint["model_state_dict"])
+            state_dict = checkpoint["model_state_dict"]
             model.load_state_dict(state_dict)
 
             model = model.to(device)
@@ -276,19 +276,14 @@ def train_single_split(config, dataset, model_name, model_folder_path, dataset_n
     '''
     Train on a single train-valid split (provided dataset is big enough) for a particular hyperparameter config. 
     '''
-    input, target = dataset[0]
-    targets = torch.stack([dataset[i][1] for i in range(len(dataset))])
+
+    train_dataset, val_dataset = dataset[0], dataset[1]
+
+    input, target = train_dataset[0]
+    targets = torch.stack([train_dataset[i][1] for i in range(len(train_dataset))])
     output_bias = torch.mean(targets)
 
     model = choose_model(model_name, input.shape, output_bias, config, model_folder_path = model_folder_path).to(device)
-
-    val_percent = 2/9 #Since we already reserved 10 percent of data for testing 
-    val_size = int(val_percent * len(dataset))
-    train_size = len(dataset) - val_size
-
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
-
-    val_targets = torch.stack([target for _, target in val_dataset])
 
     train_loader = torch.utils.data.DataLoader(
             train_dataset, batch_size= int(config["batch_size"]), shuffle = True, num_workers = num_workers, pin_memory = True, collate_fn = collate_fn)
@@ -310,7 +305,7 @@ def train_single_split(config, dataset, model_name, model_folder_path, dataset_n
     early_stop = int(config["max_epochs"] * 0.1)
 
     wandb.init(
-      project=f"{model_name}{mode}",
+      project=f"{model_name}{mode}{dataset_name}",
       config = config
       )
 
@@ -429,7 +424,7 @@ def test_best_config(test_dataset, model_name, model_file_name, model_folder_pat
         inputs, targets = [], []
         if model_name == "DeepMLP":
             inputs = test_dataset.tensors[0]
-            targets = test_dataset.tensors[0]
+            targets = test_dataset.tensors[1]
         elif model_name == "Ensemble":
             for dataset in test_dataset.datasets:
                 inputs.append(test_dataset.tensors[0])
@@ -501,7 +496,7 @@ def tune_model(data_folder_path, model_folder_path, model_name, mode = 'architec
         "config": best_config,
         "loss": best_metrics['mean_val_loss']
     }
-    with open (f'{model_folder_path}/best_config_{mode}_{model_name}.json', 'w') as f:
+    with open (f'{model_folder_path}/best_config_{mode}_{model_name}_{dataset_name}.json', 'w') as f:
         json.dump(best_settings_map, f)
 
 def tune_best_arch(model_name, data_folder_path, model_folder_path, config_name, dataset_name):
@@ -511,7 +506,7 @@ def tune_best_arch(model_name, data_folder_path, model_folder_path, config_name,
     train_val_dataset = create_model_dataset(data_folder_path, model_folder_path, model_name, dataset_name = dataset_name)
     tune_model(data_folder_path, model_folder_path, model_name, mode = 'lr', arch_config = best_model_arch_config, single_split = True)
     
-def train_best_config(model_name, data_folder_path, model_folder_path, dataset_name, config_name, mode):
+def train_best_config(model_name, data_folder_path, model_folder_path, dataset_name, config_name, mode = 'lr'):
     with open (f'{model_folder_path}/{config_name}.json', 'r') as f:
         best_config = json.load(f)
     train_val_dataset = create_model_dataset(data_folder_path, model_folder_path, model_name, dataset_name = dataset_name)
@@ -530,10 +525,11 @@ def main():
     model_type = 'Ensemble'
     model_folder_path = f'{config.MODEL_FOLDER_PATH}/{model_type}'
     # tune_model(data_folder_path, model_folder_path, model_type, single_split = True)
-    # tune_best_arch(model_type, f"{data_folder_path}", model_folder_path, config_name = f"best_config_architecture_{model_type}", dataset_name = "")
-    train_best_config(model_type, f"{data_folder_path}", model_folder_path, "", "best_config_lr_Ensemble_10_14_24", mode = 'lr')
+    tune_best_arch(model_type, f"{data_folder_path}", model_folder_path, config_name = f"best_config_architecture_{model_type}", dataset_name = "")
+    # train_best_config(model_type, f"{data_folder_path}", model_folder_path, "", "best_config_lr_Ensemble_10_19_24", mode = 'lr')
     # tune_model_arch(data_folder_path, model_folder_path, model_name, single_split = True)
     # tune_best_arch(model_name, data_folder_path, model_folder_path, 'best_config_8_28_24_b')
+
     # dataset_name = 'ena'
     # train_best_config(model_type, f"{data_folder_path}/{dataset_name}", model_folder_path, dataset_name, f'best_config_{dataset_name}')
     # dataset_name = 'dycoms'

Code/config.py
@@ -6,11 +6,12 @@ MODEL_FOLDER_PATH = "/home/groups/yzwang/gabriel_files/Machine-Learning-Cloud-Mi
 REGIONS = ["atex", "dycoms", "ena", "sgp"]
 
 # Training/Tuning Parameters
+SEED = 42069
 NUM_PROCESSES = 20
 NUM_GPUS = torch.cuda.device_count()
 NUM_CPUS = os.cpu_count() 
 NUM_WORKERS = 16 if NUM_CPUS / NUM_PROCESSES > 16 else int(NUM_CPUS / NUM_PROCESSES)
-NUM_SAMPLES = 100
+NUM_SAMPLES = 200
 REDUCTION_FACTOR = 4
 BRACKETS = 4
 GRACE_PERIOD = 2